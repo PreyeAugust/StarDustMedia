b0b31ebdf09cf97427669172b69a8bf94b2adcc5
august <kingaugust20@icloud.com>
2024-11-20 04:04:49 -0800

    more chnagess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 import Page from "../app/home/page";
+import Page1 from "../app/about/page";
+import Page2 from "../app/contactUs/page";
+import Page3 from "../app/creativeServices/page";
+import Page4 from "../app/products/page";
+import Page5 from "../app/services/page";
 
 export default function Home() {
   useEffect(() => {
@@ -21,6 +26,11 @@ export default function Home() {
   return (
     <div>
       <Page />
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
     </div>
   );
 }
