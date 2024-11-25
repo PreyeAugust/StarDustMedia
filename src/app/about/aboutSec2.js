8371fbaaaf3b03c14bbdb933148ab1e61830b16f
august <kingaugust20@icloud.com>
2024-11-25 06:34:35 -0800

    more change

--- src/app/about/aboutSec2.js
+++ src/app/about/aboutSec2.js
@@ -1,44 +1,138 @@
 import React from "react";
 import "./about.css";
-
-
+import Image from "next/image";
+import Img1 from "@/../public/AboutValue.jpg";
 
 export default function aboutSec2() {
-
   return (
-    <div className="py-2">
-      <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-20">
-          {/* first col */}
-          <div className=" about2First py-10 pl-5 text-white rounded-3xl relative h-[480px]  flex items-center ">
-            <div>
-              <div data-aos="zoom-in" className="mb-4">
-                
-                <p className="text-4xl xl:text-5xl font-bold mb-2 about2ps uppercase">
-                  {" "}
-                 Our Values
-                </p>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="flex flex-wrap w-full">
+          <div className="lg:w-3/5 md:w-1/2 md:pr-10 md:py-6">
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              </div>
+              <div className="AboutValueIMG flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5 "
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                </svg>
+              </div>
+              <div className="AboutValueIMG flex-grow pl-4">
+                <h2 className="font-bold title-font text-lg text-gray-900 mb-1 tracking-wider">
+                  OUR VALUES
+                </h2>
+              </div>
+            </div>
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              </div>
+              <div className="AboutValueIMG flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  INNOVATION
+                </h2>
+              </div>
+            </div>
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              </div>
+              <div className="AboutValueIMG flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx="12" cy="5" r="3"></circle>
+                  <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                </svg>
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  QUALITY
+                </h2>
+              </div>
+            </div>
+            <div className="flex relative pb-12">
+              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              </div>
+              <div className="AboutValueIMG flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  SOCIAL IMPACT
+                </h2>
+              </div>
+            </div>
+            <div className="flex relative">
+              <div className="AboutValueIMG flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5 "
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+              </div>
+              <div className="flex-grow pl-4">
+                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  CLIENT SATISFACTION
+                </h2>
               </div>
             </div>
           </div>
-          {/* second col */}
-         
-          {/* third col */}
 
-          <div className="about2First3 sm:col-span-2 py-10 pl-5 text-white rounded-3xl relative h-[480px]  flex items-center ">
-            <div>
-              <div data-aos="zoom-in" className="mb-1">
-                
-                <p className="text-2xl font-semibold mb-[2px] about2ps">Innovation</p>
-                <p className="text-2xl font-semibold mb-[2px] about2ps">Quality</p>
-                <p className="text-2xl font-semibold mb-[2px] about2ps">Social Impact</p>
-                <p className="text-2xl font-semibold mb-[2px] about2ps">Client Satisfaction</p>
-                
-              </div>
-            </div>
-          </div>
+          <Image
+            src={Img1}
+            className="lg:w-2/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
+            alt="step"
+          />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
