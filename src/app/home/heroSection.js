9d81dd029473e718984b9a91a8232e1ffcb588e1
august <kingaugust20@icloud.com>
2024-12-07 10:06:52 -0800

    origin

--- src/app/home/heroSection.js
+++ src/app/home/heroSection.js
@@ -7,10 +7,10 @@ export default function heroSection() {
     <main className="heroMain flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover">
       <div className="absolute w-full top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className=" container p-5 text-white z-[2] mt-[10rem]">
-        <h1 className="uppercase font-serif text-3xl">
+        <h1 className="content uppercase font-serif text-3xl">
           Imagination to reality
         </h1>
-        <p className="py-5 text-md font-serif ">
+        <p className=" content-P py-5 text-md font-serif ">
           {" "}
           A dynamic creative agency dedicated to inspiring young minds and
           empowering individuals through innovative solutions and captivating
