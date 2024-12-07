3b518a9289259fba78ca3a76c0102751a198e9d6
august <kingaugust20@icloud.com>
2024-12-07 10:35:45 -0800

    ok

--- src/app/home/heroSection.js
+++ src/app/home/heroSection.js
@@ -4,13 +4,13 @@ import "./home.css";
 
 export default function heroSection() {
   return (
-    <main className="heroMain flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-homeHero">
+    <main className=" flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-homeHero">
       <div className="absolute w-full top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className=" container p-5 text-white z-[2] mt-[10rem]">
-        <h1 className="content uppercase font-serif ">
+        <h1 className="uppercase font-serif text-2xl">
           Imagination to reality
         </h1>
-        <p className=" content-P py-5 font-serif ">
+        <p className="py-5 text-md font-serif ">
           {" "}
           A dynamic creative agency dedicated to inspiring young minds and
           empowering individuals through innovative solutions and captivating
