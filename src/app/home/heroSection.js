9beedfac17409a21f89a3cb517781348669f3b3c
august <kingaugust20@icloud.com>
2024-12-09 01:17:12 -0800

    ok

--- src/app/home/heroSection.js
+++ src/app/home/heroSection.js
@@ -4,7 +4,7 @@ import "./home.css";
 
 export default function heroSection() {
   return (
-    <main className="heroMain flex items-center justify-center mb-12 bg-fixed bg-center bg-cover custom-homeHero">
+    <main className="heroMain flex items-center justify-center mb-12  bg-center bg-cover custom-homeHero">
       <div className=" container p-5 text-white z-[2] mt-[10rem]">
         <h1 className="uppercase font-serif text-2xl">
           Imagination to reality
